6c06a7fbfcdab8fa080f69abeaeb92b003ca52c8
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2020-09-22 22:52:45 +0530

    Update mosby.py

--- mosby.py
+++ mosby.py
@@ -3,6 +3,7 @@ import random
 import discord   
 import urllib.request
 import re  
+import youtube_dl
 from discord.ext import commands 
 import os
 from discord.utils import get
@@ -16,7 +17,7 @@ def searchQuery(str,num):
     video_id=re.findall(r"watch\?v=(\S{11})",html.read().decode())
     for i in range(len(video_id)):
         video_id[i]=f"https://www.youtube.com/watch?v={video_id[i]}"
-    print(video_id[num])
+    return(video_id[num])
 
 def isWord(lst,wrd):
     fg=False
@@ -29,33 +30,15 @@ def isWord(lst,wrd):
 #Lists
 himym=["https://youtu.be/MYnhPtsRQjA", "https://youtu.be/v73s7SCWzvs","https://youtu.be/HpLsYgXpIws","https://youtu.be/xWa-QVAd7gM","https://youtu.be/zwYOXsb7FbI","https://youtu.be/UImXzJsId2w","https://youtu.be/Dcngel9VyIU","https://youtu.be/fiuu245RpwQ","https://youtu.be/8Bw5Z0rBteY","https://youtu.be/I90B8tQ9qRA","https://youtu.be/yvXIpvh9hvI","https://youtu.be/6b2AGAB2pA0"]
 
-brocode=["""Article 1 : Bro’s before ho’s
-The bond between two men is stronger than the bond between a man and a woman because, on average, men are stronger than women. That’s just science.
-""",
-'Article 2: A Bro is always entitled to do something stupid, as long as the rest of his Bros are all doing it.',
+
+brocode=['Article I : Bros before hoes.',
+'Article II : A bro is always entitled to do something stupid as long as the rest of his bros are all doing it.',
+'Article III : If a bro gets a dog, it must be at least as tall as his knee when full grown.\nCorollary: Naming a lap dog after a pro wrestler or character from an action flick does not absolve said bro from this article.',
+'Article IV : A bro never divulges the existence of the bro code to a woman. It is a sacred document not to be shared with chicks for any reason. No, not even that reason.\nNote:\nIf you are a woman reading this, first, let me apologize: it was never my intention for this book to contain so much math.\nSecond, I urge you to look at this document for what it is – a piece of fiction meant to entertain a broad audience though the prism if stereotypical gender differences. I mean, sometimes it really is like we’re from different planets! Clearly, no real person would actually believe or adhere to the vulgar rules contained within. *Those boots are adorable, b-t-dub.',
+'Article V : Whether a bro cares about sports or not, a bro cares about sports.',
+'Article VI : A bro shall not lollygag if he must get naked in front of other bros, such as in a gym locker room. Corollary: If a bro gets naked in a locker room all other bros shall pretend that nothing out of the ordinary is happening, while at the same time immediately averting their eyes. When in doubt, remember the old adage, if a towel drops to the floor, so should your eyes.',
 """
-Article 3 : If a Bro gets a dog, it must be at least as tall as his knee when full-grown.
-Corollary: Naming a lapdog after a pro wrestler or a character from a Steve McQueen movie does not absolve a Bro from the spirit of this article.
-""",
-"""
-Article 4 : A Bro never divulges the existance of The Bro Code to a woman. It is a sacred document not to be shared with chicks for any reason… no, not even that reason.
-Note:
-If you are a woman reading this, first, let me apologize: it was never my intention for this book to contain so much math.
-Second, I urge you to look at this document for what it is – a piece of fiction meant to entertain a broad audience though the prism if stereotypical gender differences. I mean, sometimes it really is like we’re from different planets! Clearly, no real person would actually believe or adhere to the vulgar rules contained within. *Those boots are adorable, b-t-dub.
-""",
-"""
-Article 5 : Whether he cares about sports or not, a Bro cares about sports.
-""",
-"""
-Article 6 : A Bro shall not lollygag if he must get naked in front of other Bros in a gym locker room
-COROLLARY: If a bro gets naked in a locker room, all other Bros shall pretend that nothing out of the ordinary is happening while, at the same time, immediately averting their eyes. When in doubt, remember the old adage: “If a towel drops to the floor, so should your eyes”.
-""",
-"""
-Article 7 : A Bro never admits he can’t drive, even after an accident.
-""",
-"""
-A Bro never sends a greeting card to another Bro
-There are no sentiments between Bros that cannot be articulated through the convenience and emotional distance of electronic mail. The following are a few emails for any Brocassion that succinctly get the message accross without costnig you the trouble and expense of having to find and then send an actual greeting card.
+Article VII : A bro never sends a greeting card to another bro.
 
 EMAILS FOR ANY BROCASSION:
 
@@ -99,17 +82,24 @@ Subject: N/A
 N/A
 ----------------------------------
 """,
-'ARTICLE 24: "When wearing a baseball cap, a Bro may position the brim at either 12 or 6 o’clock. All other angles are reserved for rappers and the handicapped.”',
+'Article VIII : A bro never admits he can’t drive stick, even after an accident.',
+'Article IX : Should a Bro lose a body part due to an accident or illness, his fellow Bros will not make lame jokes such as “Gimme three!” or “Wowm quitting your job like that really took a lot of ball”. It’s still a high five and that Bro still has a lot of balls… metaphorically speaking, of course',
 """
-ARTICLE 100: "When pulling up to a stoplight, a Bro lowers his window so that all might enjoy his music selection."
-COROLLARY : "If there happens to be a hot chick driving the car next to the Bro, the Bro shall put his sunglasses down to get a better look. If he's not wearing his sunglasses, he will first put them on, then pull down to get a better look.”
+Article X : A Bro will drop whatever he’s doing and rush to help his Bro dump a chick
+
+It’s normal for a Bro to get confused and disorientated when dumping a chick. For some reason he’s worried she’ll become agitated or even violent after he calmly explains his desire to have sex with her friends. This is when a Bro most needs his Bro to remind him that there are plenty more chicks in the ocean, and that a breakup need not be hazardous, stressful, or even time-consuming.
+
+SIDE-BRO: HOW TO DUMP A CHICK IN SIX WORDS OR LESS
+
+“Maybe try a side salad instead”
+“Cute! You’re growing a mustache, too!”
+“She looks like a younger you”
+“I will finance a boob job”
+“Sorry I threw out your shoes”
+“Your sister let me do that”
 """,
-'ARTICLE 54 : "A Bro is required to go out with his Bros on St. Patty’s Day and other official Bro holidays, including Halloween, New Year’s Eve, and Desperation Day (February 13).”',
-'ARTICLE 85 : "If a Bro buys a new car, he is required to pop the hood when showing it off to his Bros."\nCOROLLARY: "His Bros are required to whistle, even if they have no idea what they’re whistling at.” ',
-'ARTICLE 41 : "A Bro never cries."\nEXCEPTIONS: "Watching Field of Dreams, E.T., or a sports legend retire."',
-'ARTICLE 120 : "A Bro always calls another Bro by his last name."   \nEXCEPTION: "If a Bro’s last name is also a racial epithet.” ',
-'ARTICLE 130 : "If a Bro learns another Bro has been in a traffic accident, he must first ask what type of car he collided with and whether it got totaled before asking if his Bro is okay.” '
-]
+"Article XI : A Bro may ask his Bro(s) to help him move, but only after first disclosing an honest estimate on both time commitment and number of large pieces of furniture. If the Bro has vastly underestimated either, his Bros retain the right to leave his possessions where they are – in most cases, stuck in the doorway.",
+'Article XII : Bros do not share desert.', 'Article XIII : All bros shall dub one of their bros their wingman.', 'Article XIV : If a chick inquires about a bros sexual history, a bro shall honor the ‘Brode of Silence’ and play dumb. Better to have women think all men are dumb than to tell the truth.', 'Article XV : A bro never dances with his hands above his head.', 'Article XVI : A bro should be able to at any time recite the following reigning champions: Super Bowl and World Series.', 'Article XVII : A bro shall be kind and courteous to his coworkers, unless they are beneath him on the pyramid of screaming.', 'Article XVIII : If a bro spearheads a beverage run, he is entitled to any extra money accrued after canvassing the group.', 'Article XIX : A bro shall not sleep with another bro’s sister. However, a bro shall not get angry if another bro says “dude, your sister’s hot!”. Corollary: it’s probably better for everyone if bros just hide pictures of their sister/s when other bros are coming over.', 'Article XX : A bro respects his bros in the military because they have chosen to defend the nation, but more to the point because they can kick his @/$ six ways to Sunday.', 'Article XXI : A bro never exchanges notes about an antithetical bro’s smoking hot girlfriend, even if the bro with the hot girlfriend attempts to bait the bro by saying “She’s hot, huh?”. A bro shall remain silent on the subject because, in this situation, he should be the only one bateing.', 'Article XXII : There is no law that prohibits a woman from being a bro.', 'Article XXIII : When flipping through channels with his bros, a bro is not allowed to skip past a program featuring sports.', 'Article XXIV : When wearing a baseball cap, a bro way position the brim at either twelve or six o’clock. All other angles are reserved for rappers and the handicapped. If a bro fits under either of these classifications, he is excused from this article.', 'Article XXV : A bro doesn’t let another bro get a tattoo. Including but not limited to a girls name or tramp stamp.', 'Article XXVI : Unless he has children, a bro shall not wear his cellphone on a belt clip.', 'Article XXVII : A bro never removes his shirt in front of another bro unless at a resort, pool, or beach.', 'Article XXVIII : A bro will, in a timely manner, alert his bro to the existence of a girl fight.', 'Article XXIX : If two bros decide to catch a movie together, they may not attend a screening that begins after 4:40 PM. Also, despite cost saving, they shall not share a tub of popcorn.', 'Article XXX : A bro doesn’t comparison shop.', 'Article XXXI : When on the prowl, a bro hits on the hottest girl first because you just never know.', 'Article XXXII : A bro doesn’t allow another bro to get married until he is at least 30.', 'Article XXXIII : When in a public restroom, a bro:\n1.) looks straight ahead while using the urinal,\n2.) makes the obligatory comment ‘what is this, a chick’s restroom?’ if there are more than two dudes waiting to pee, and\n3.) Attempts to basketball toss his used paper towels into the trash can (rebounding is optional).', 'Article XXXIV : Bros cannot make eye contact during a devil’s three way (two dudes).', 'Article XXXV : A bro never rents a chick flick.', 'Article XXXVI : When questioned in the company of women, a bro always decries fake breasts.', 'Article XXXVII : A bro is under no obligation to open a door for anyone.', 'Article XXXVIII : Even in a fight to the death, a bro never punches another bro in the groin.', 'Article XXXIX : When a bro gets a chick’s number, he waits at least 96 hours before calling her.', 'Article XL : Should a bro become stricken with engagement, his bros shall stage an intervention and attempt to heal him.', 'Article XLI : A bro never cries. Exceptions: watching Field of Dreams, ET, or a sports legend retire.', 'Article XLII : Upon greeting another bro, a bro may engage a high five, fist bump, or bro hug, but never a full embrace.', 'Article XLIII : A bro loves his country, unless that country isn’t America.', 'Article XLIV : A bro never applies sunscreen on another bro.', 'Article XLV : A bro never wears jeans to a strip club.', 'Article XLVI : If a bro is seated next to some dude stuck in the middle seat on an airplane, he shall yield him all of their shared armrests unless the dude has;', 'A) Taken his shoes off : B) Is snoring', 'C) Makes the bro get up more than once to use the lavatory : D) Purchased headphones after they announced the in flight movie is 27 dresses.', 'Article XLVII : A bro never wears pink. Corollary: when wearing a shirt to save the boobs.', 'Article XLVIII : A bro never publicly reveals how many chicks he or another bro has banged.', 'Article IL : When asked ‘do you need help’ a bro automatically responds with ‘no’. Exceptions: when involving and expensive car or TV.', 'Article L : If a bro should accidentally stroke another bro’s undercarriage with his arm while walking, both bros silently agree to continue walking as if it never happened.', 'Article LI : A bro checks out another bro’s blind date and reports back with a thumbs up or thumbs down.', 'Article LII : A bro is not required to remember another bro’s birthday.', 'Article LIII : Even in a drought, a bro flushes.', 'Article LIV : A bro is required to go out with his bros on St. Party’s day and other official bro holidays.', 'Article LV : Even in an emergency that requires a tourniquet, a bro never borrows from or lends clothes to another bro.', 'Article LVI : A bro is required to alert one and only one bro if the bro-chick ratio at a party falls below 1:1', 'Article LVII : A bro never reveals to score of a sporting event to a bro unless that bro has thrice confirmed he wants to hear it.', 'Article LVIII : A bro doesn’t grow a mustache.', 'Article LIX : A bro always posts bail for another bro unless its out if state or like crazy expensive.', 'Article LX : A bro shall honor thy mother and father for they were once bro and chick. However, a bro never thinks of his parents as bro and chick.', 'Article LXI : If for any reason a bro becomes aware of a bro and chick’s anniversary he will endeavor to make this information to the bro in the relationship whether the first bro thinks the second bro is aware of this or not.', 'Article LXII : In the event that two bros lock on to the same target, the bro who calls dubs first has dibs.', 'Article LXIII : A bro will make any and all efforts to provide his bro with protection.', 'Article LXIV : A bro must provide must provide a ticket to an event he is attending if said event involves the second bro’s favorite sports team in a playoff scenario.', 'Article LXV : A bro must always reciprocate a round of drinks among bros.', 'Article LXVI : If a bro suffers pain due to the permanent dissolution of a relationship with his lady friend, his bros shall offer no more than an ‘aw, that sucks man.’ And copious quantities of a preferred beverage.', 'Article LXVII : Should a bro pick up a guitar at a party and begin to play, another bro shall point out that he is a tool.', 'Article LXVIII : If a bro is on a ‘hot streak’ another bro will do anything he can to ensure its longevity, even if that includes jeopardizing his own records, the missing of work, or generating the imminent fear the world will soon end. Exception: Dry spell trumps hot streak.', 'Article LXIX : Duh.', 'Article LXX : A bro will drive another bro to the airport or pick him up, but never both for the same trip.', 'Article LXXI : As a courtesy to bros the world over, a bro never brings more than two other bros to a party.', 'Article LXXII : A bro never spellchecks.', 'Article LXXIII : When a group of bros are in a restaurant, each shall engage in jockeying the bill, regardless of affordability.', 'Article LXXIV : At a red light, a bro inches as close to the rear bumper of the car in front of him.', 'Article LXXV : A bro automatically promotes another bro’s job status when introducing him to a chick.', 'Article LXXVI : If a bro is on the phone with a chick in front of his bros and for whatever reason desires to day ‘I love you’ he must first excuse himself from the room or deploy a subsonic tone.', 'Article LXXVII : Bros don’t cuddle.', 'Article LXXVIII : A bro shall never rack jack his wingman.', 'Article LXXIX : At a wedding, a bro always first seeks out an open bar for his group of bros.', 'Article LXXX : A bro shall make every effort possible to aid a bro in ‘riding the tricycle’ short of completing the tricycle.', 'Article LXXXI : A bro leaves the toilet seat up for his bros.', 'Article LXXXII : If two bros get into a heated argument about something and one says something out of line, the other shall not expect him to take it back or apologize.']
 
 sunflowers=['Aah , _sunflowers_ !', "It's funny how something as delicate as Sunflowers could hurt so much :)", "Life's been such a mess of Sunflowers and Daffodils :)" ]
 daffodils=['Aah , "_If Daffodils Were A Person_ !"', "It's funny how something as delicate as Daffodils could hurt so much :)", "Life's been such a mess of Sunflowers and Daffodils :)" ]
@@ -129,7 +119,7 @@ async def on_message(message):
     if message.author != client.user :
         mess=mess.lower()
         getcmd=mess.split()
-        if isWord(getcmd,"sad") :
+        if isWord(getcmd,"sad") and isWord(getcmd,"feel") :
             await general_channel.send("Did I just hear sad ? Wtf do you mean by that <@{}>? When I am sad , I just stop being sad and be awesome instead !".format(message.author.id))
             await message.author.send("Bro You Okay ?\nI know we don't talk much but I love you okay ? You are important to me ! And This is not just the Bot saying :)\n Here Have A Playlist : https://spoti.fi/3kAUwcN")     
         if isWord(getcmd,"sunflowers") or isWord(getcmd,"sunflower") :
@@ -215,7 +205,50 @@ async def leave(ctx):
         await ctx.send(f"Imma Ight Head Out Of {channel} Voice")
     else:
         voice = await ctx.send("Bruh am not even in a Voice Channel !")
+
+@client.command(name="playsong", pass_context=True)
+async def playsong(ctx,str1,n):
+    name=""
+    song_there=os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+            print("Removed Song File")
+    except PermissionError:
+        print("The Song Is On")
+        await ctx.send("Bro I think the song is already playing !")
+        return
     
+    await ctx.send("Firing That Up Now !")
+    
+    voice = get(client.voice_clients, guild=ctx.guild)
+    ydl_opts={
+        'format': 'bestaudio/best',
+        'postprocessors':[{
+            'key':'FFmpegExtractAudio',
+            'preferredcodec':'mp3',
+            'preferredquality':'192',
+        }],
+    }
+    v_url=searchQuery(str1,int(n))
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+          print("Downloading Song")
+          ydl.download([v_url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name=file
+            print(f"Renamed File {file}")
+            os.rename(file,"song.mp3")
+    
+    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print("Vibing"))
+    voice.souce=discord.PCMVolumeTransformer(voice.source)
+    voice.souce.volume=0.07
+
+    await ctx.send(f"Playing {v_url}")
+        
+        
     
 
 #Run The Bot
